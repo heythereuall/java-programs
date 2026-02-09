fb3877a14d8ef218092157ed12f23c51d1147e60
Aditi Jaiswal <95347598+heythereuall@users.noreply.github.com>
2026-02-10 00:04:01 +0530

    .

---
+++ PrintArgs.java
@@ -0,0 +1,6 @@
+class PrintArgs {
+    public static void main(String args[]) {
+        System.out.println("First value: " + args[0]);
+        System.out.println("Second value: " + args[1]);
+    }
+}
