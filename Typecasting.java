7b183a5f375d69494471642c5e2ad304d9ea8bf5
Aditi Jaiswal <95347598+heythereuall@users.noreply.github.com>
2026-02-10 22:45:15 +0530

    .

---
+++ Typecasting.java
@@ -0,0 +1,12 @@
+class TypeCasting {
+    public static void main(String[] args) {
+        int a = 10;
+        double b = a;
+
+        double x = 9.8;
+        int y = (int) x;
+
+        System.out.println("int to double: " + b);
+        System.out.println("double to int: " + y);
+    }
+}
