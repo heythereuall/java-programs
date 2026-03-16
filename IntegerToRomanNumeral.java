79028a37b593e770b3a6aae790c92a60ba4411e5
Aditi Jaiswal <95347598+heythereuall@users.noreply.github.com>
2026-03-17 01:05:23 +0530

    .

---
+++ IntegerToRomanNumeral.java
@@ -0,0 +1,7 @@
+class Roman{
+    public static void main(String args[]){
+        int num=58;
+        String[] r={"","I","II","III","IV","V","VI","VII","VIII","IX"};
+        System.out.println("Roman: L"+r[num-50]);
+    }
+}
