030b65aafb33ffd29199908a346eb628b726bb2c
Aditi Jaiswal <95347598+heythereuall@users.noreply.github.com>
2026-03-17 00:47:07 +0530

    .

---
+++ ConstructorChainingUsingsuper().java
@@ -0,0 +1,18 @@
+class A{
+    A(){
+        System.out.println("Constructor of A");
+    }
+}
+
+class B extends A{
+    B(){
+        super();
+        System.out.println("Constructor of B");
+    }
+}
+
+public class Main{
+    public static void main(String args[]){
+        B obj = new B();
+    }
+}
