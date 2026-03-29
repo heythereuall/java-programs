e92823a8a6dcaab8505b1817890f55ae604e3b2b
Aditi Jaiswal <95347598+heythereuall@users.noreply.github.com>
2026-03-30 01:32:15 +0530

    .

---
+++ NullPointerException.java
@@ -0,0 +1,12 @@
+public class Main{
+    public static void main(String args[]){
+        try{
+            String s=null;
+            System.out.println(s.length());
+        }
+        catch(Exception e){
+            System.out.println(e);
+            System.out.println(e.getClass());
+        }
+    }
+}
